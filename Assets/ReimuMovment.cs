800f095205754dbcccb1884ca66b2c566e8ce20b
Carlos <carloseduardo108090@hotmail.com>
2020-04-27 22:14:35 -0300

    first commit

---
+++ Assets/ReimuMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReimuMovment : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator anima;
+
+    Vector2 movment;
+    // Update is called once per frame
+    void Update()
+    {
+        movment.x = Input.GetAxis("Horizontal");
+        movment.y = Input.GetAxis("Vertical");
+
+        anima.SetFloat("Horizontal", movment.x);
+        anima.SetFloat("Vertical", movment.y);
+        anima.SetFloat("Speed", movment.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movment * moveSpeed * Time.fixedDeltaTime);
+    }
+}
